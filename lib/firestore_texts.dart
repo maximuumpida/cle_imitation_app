835037dfb89c81af1cabcfdfffe4650e650dc582
pida <parupunte012@gmail.com>
2024-05-21 19:34:51 +0900

    firebasetextrenew

--- lib/firestore_texts.dart
+++ lib/firestore_texts.dart
@@ -4,13 +4,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreTexts extends StatelessWidget {
   const FirestoreTexts({super.key});
 
-  Future<String> getDoc() async {
-    DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore
-        .instance
+  void updateDoc() async {
+    FirebaseFirestore.instance
         .collection('tani')
         .doc('8knF62NaXCeXNXTLXkWx')
-        .get();
-    return snapshot.data()!['body'];
+        .update({'body': 'テキストを更新しました。'});
   }
 
  @override
@@ -18,19 +16,13 @@ class FirestoreTexts extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('タイトル')),
       body: Center(
-        child: FutureBuilder(
-          future: getDoc(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return Text(snapshot.data);
-            } else {
-              return const Center(
-                child: Text('データが取得できていません'),
-              );
-            }
-          },
-        ),
+           child: GestureDetector(
+            onTap: () {
+              updateDoc();
+            },
+            child: const Text('タップで更新')),
       ),
     );
-  }
-}
+            }
+          }
+        
